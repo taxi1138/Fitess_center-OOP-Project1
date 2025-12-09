ed131f7b4c54dec7565738be5302aac4df44617c
Matvei Kurdiukov <vconopla@mail.ru>
2025-12-09 16:24:27 +0500

    Initial project structure

---
+++ src/com/Matthew/Fitness_center/Main.java
@@ -0,0 +1,11 @@
+package com.Matthew.Fitness_center;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome. In the future perspective this is a gym management system, which should help to comfortably run such businesses as gym or fitness center.");
+        System.out.println("What exactly will it do? : This system helps manage gym members and their memberships.\n" +
+                "You can register new clients and track their attendance.\n" +
+                "It also allows staff to manage membership plans and payments.\n" +
+                "More features will be added as the system develops.");
+    }
+}
