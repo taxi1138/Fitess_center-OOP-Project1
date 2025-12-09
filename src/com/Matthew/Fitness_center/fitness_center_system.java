e571c99e1ad43d43a142780e40ae80f2ef6c5862
Matvei Kurdiukov <vconopla@mail.ru>
2025-12-09 20:35:40 +0500

    Updated Main.java with new features

--- src/com/Matthew/Fitness_center/fitness_center_system.java
+++
@@ -1,8 +0,0 @@
-
-void main() {
-    IO.println(String.format("Welcome. In the future perpective this is a gym management system, which should help to comfortably run such businesses as gym or fitness center."));
-    IO.println(String.format("What exactly will it do? : This system helps manage gym members and their memberships.\n" +
-            "You can register new clients and track their attendance.\n" +
-            "It also allows staff to manage membership plans and payments.\n" +
-            "More features will be added as the system develops."));
-}
